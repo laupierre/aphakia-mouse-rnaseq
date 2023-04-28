dedfbfc21914f73a932b239acca1c6d2455a2f7c
laupierre <laupierre@users.noreply.github.com>
2023-04-28 12:28:49 +0200

    Update deg_filtered_analysis.R

--- deg_filtered_analysis.R
+++ deg_filtered_analysis.R
@@ -40,7 +40,7 @@ pheno <- read.delim ("Moratalla phenodata 220303.txt")
 pheno <- pheno[pheno$area == "SN", ]
 pheno <- pheno[pheno$experiment ==1, ]
 
-## WT are WT1SN, WT2SN, (WT4SN), WT5SN
+## WT are WT1SN, WT2SN, WT4SN, WT5SN
 ## low AK are AK2SN, AK4SN, AK7SN
 ## high AK are AK5SN, AK6SN, AK8SN, AK9SN
 
@@ -66,6 +66,71 @@ idx <- match (colnames (a), pheno$sample)
 pheno <- pheno[idx, ]
 
 stopifnot (colnames (a) == pheno$sample)
+
+
+
+dds <- DESeqDataSetFromMatrix(countData = round (a), colData = pheno, design = ~ genotype)
+
+keep <- rowSums(counts(dds) >= 10) >= 5
+dds <- dds[keep,]
+dds
+
+dds <- DESeq(dds)
+
+## Population 1
+res <- results(dds, contrast=c("genotype", "AK1", "WT1"))
+
+res <- merge (data.frame (res), counts (dds), by="row.names")
+res <- merge (res, annot, by.x="Row.names", by.y="Geneid")
+colnames (res)[1] <- "Geneid"
+res <- res[order (res$padj), ]
+
+# Sanity check
+res[res$gene_name == "Pitx3", ] 
+
+write.xlsx (res, "aphakia population 1 vs WT mice comparison.xlsx", rowNames=F)
+
+## heatmap plot
+# Th, DDC, NR4A2, SLC6A3 (known target), SNCA, Pitx3, SLC18A2 (known target, see doi: 10.1111/j.1471-4159.2009.06404.x), Tcf7l2
+select <- c("ENSMUSG00000000214.12", "ENSMUSG00000020182.17", "ENSMUSG00000026826.14", "ENSMUSG00000021609.7", "ENSMUSG00000025889.14", "ENSMUSG00000025229.16", "ENSMUSG00000025094.9", "ENSMUSG00000024985.21")
+
+df <- as.data.frame(colData(dds)[,c("genotype","sample")])
+
+pdf ("Heatmap filtered plot.pdf")
+pheatmap( log2 (counts(dds,normalized=TRUE)+1) [row.names (counts(dds)) %in% select, ], cluster_rows=FALSE, show_rownames=TRUE, cluster_cols=FALSE, annotation_col=df)
+dev.off ()
+
+
+## Population 2
+res <- results(dds, contrast=c("genotype", "AK2", "WT1"))
+
+res <- merge (data.frame (res), counts (dds), by="row.names")
+res <- merge (res, annot, by.x="Row.names", by.y="Geneid")
+colnames (res)[1] <- "Geneid"
+res <- res[order (res$padj), ]
+
+# Sanity check
+res[res$gene_name == "Pitx3", ] 
+
+write.xlsx (res, "aphakia population 2 vs WT mice comparison.xlsx", rowNames=F)
+
+
+## PCA plot
+vsd <- vst(dds, blind=FALSE)
+pcaData <- plotPCA(vsd, intgroup=c("genotype", "sample"), returnData=TRUE)
+percentVar <- round(100 * attr(pcaData, "percentVar"))
+
+ggplot(pcaData, aes(PC1, PC2, color=genotype, label=sample)) +
+  		geom_point(size=3) +
+  		xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  		ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
+  		geom_text_repel()  + 
+		  coord_fixed () 
+
+ggsave ("PCA filtered plot.pdf")
+
+
+
 
 
 
