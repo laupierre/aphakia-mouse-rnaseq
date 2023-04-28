60cd41256716d2f6bdff52bfde781d47e091449f
laupierre <laupierre@users.noreply.github.com>
2023-04-28 13:46:55 +0200

    Update deg_unfiltered_analysis.R

--- deg_unfiltered_analysis.R
+++ deg_unfiltered_analysis.R
@@ -55,6 +55,7 @@ dds <- DESeq(dds)
 res <- results(dds, contrast=c("genotype", "AK", "WT"))
 
 res <- merge (data.frame (res), counts (dds), by="row.names")
+#res <- merge (data.frame (res), round (counts (dds, normalized=TRUE)), by="row.names")
 res <- merge (res, annot, by.x="Row.names", by.y="Geneid")
 colnames (res)[1] <- "Geneid"
 res <- res[order (res$padj), ]
